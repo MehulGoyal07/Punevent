821d97921d4405d910f2ad2ff84d0ef230ec5e13
MehulGoyal07 <mehulgoyal107@gmail.com>
2024-09-29 18:23:15 +0530

    homepage-modifications

--- scripts/app.js
+++ scripts/app.js
@@ -79,3 +79,80 @@ function sendwhatsapp() {
         prompt("Enter all the details")
     }
 }
+
+window.addEventListener('scroll', function() {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+// JavaScript for opening and closing mobile menu
+const menuBars = document.getElementById('menu-bars');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuBars.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const counters = document.querySelectorAll('.counter');
+    const speed = 200; // Adjust the speed of the count
+
+    counters.forEach(counter => {
+        const updateCount = () => {
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+
+            const increment = target / speed;
+
+            if (count < target) {
+                counter.innerText = Math.ceil(count + increment);
+                setTimeout(updateCount, 20);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        const isVisible = (elem) => {
+            const { top, bottom } = elem.getBoundingClientRect();
+            const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+            return (
+                (top > 0 || bottom > 0) && top < vHeight
+            );
+        };
+
+        const startCounterOnScroll = () => {
+            if (isVisible(counter)) {
+                updateCount();
+                window.removeEventListener('scroll', startCounterOnScroll);
+            }
+        };
+
+        window.addEventListener('scroll', startCounterOnScroll);
+    });
+});
+
+var reviewSwiper = new Swiper('.review-slider', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 3,
+    spaceBetween: 20,
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        480: {
+            slidesPerView: 1,
+        },
+    },
+});
